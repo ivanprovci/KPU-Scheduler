45a8360f211e23eef09c788f0a738fcd038a285f
ivanprovci <ivanprovci@yahoo.com>
2024-07-24 17:07:38 -0700

    'Delete Semester' & 'Update Semester Name' added

--- routes/semesters.js
+++ routes/semesters.js
@@ -1,4 +1,9 @@
-const { getAllSemesters, createSemester } = require("../db/semester.js")
+const {
+	getAllSemesters,
+	createSemester,
+	deleteSemesterByID,
+	updateSemesterByID,
+} = require("../db/semester.js")
 
 const express = require("express")
 const router = express.Router()
@@ -32,6 +37,48 @@ router.post("/semesters", async (req, res) => {
 		})
 	} catch (error) {
 		res.status(500).json({ message: "Error creating semester." })
+	}
+})
+
+// delete a semester
+router.delete("/semesters", async (req, res) => {
+	const semesterId = req.body.semesterId
+
+	if (!semesterId || semesterId.trim() === "") {
+		return res.status(400).json({ message: "Semester ID cannot be empty." })
+	}
+
+	try {
+		await deleteSemesterByID(semesterId)
+		res.status(200).json({
+			message: "Semester was deleted successfully.",
+		})
+	} catch (error) {
+		res.status(500).json({ message: "Error deleting semester." })
+	}
+})
+
+// update semester name
+router.patch("/semesters", async (req, res) => {
+	const semesterId = req.body.semesterId
+	const newSemesterName = req.body.newSemesterName
+
+	if (!semesterId || semesterId.trim() === "") {
+		return res.status(400).json({ message: "Semester ID cannot be empty." })
+	}
+	if (!newSemesterName || newSemesterName.trim() === "") {
+		return res
+			.status(400)
+			.json({ message: "Semester name cannot be empty." })
+	}
+
+	try {
+		await updateSemesterByID(semesterId, { Name: newSemesterName })
+		res.status(200).json({
+			message: "Semester name was updated successfully.",
+		})
+	} catch (error) {
+		res.status(500).json({ message: "Error updating semester name." })
 	}
 })
 
